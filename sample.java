478ea946d8e11397a0f8dc206d2cd5c0fa50620c
tarak35 <61867788+tarak35@users.noreply.github.com>
2021-09-09 11:22:21 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,5 +1,5 @@
 public class sample{
 	public static void main(String[] args){
-		System.out.println("Jenkins Integration with git");
+		System.out.println("Jenkins Integration with git successfully completed");
 	}
 }
